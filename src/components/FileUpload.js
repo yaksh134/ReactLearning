f11b7a0646f01c4e9d1e3ec8d1007b7f8449eb0f
yaksh134 <yaksh134@gmail.com>
2022-04-20 00:05:24 +0530

    GeoLocation

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 
 export const FileUpload = () => {
 
+    const style = {
+        color : "red"
+    }
+
     const [name, setname] = useState('')
     const [file, setfile] = useState()
     const [multiplefile, setmultiplefile] = useState([])
@@ -16,12 +20,27 @@ export const FileUpload = () => {
     const singlefilehandler = (e) =>{
         console.log(e.target.files)
         setfile(e.target.files[0])
+        console.log(e.target.files[0].size)
+        e.target.files[0].size > 800000 ? document.getElementById("error").innerHTML = "File size excedded" : document.getElementById("error").innerHTML = ""
     }
+
+    const loc = (e) =>{
+        e.preventDefault()
+        navigator.geolocation.getCurrentPosition(position=>{
+            console.log("Lattitue is : ",position.coords.latitude)
+            console.log("Longitude is : ",position.coords.longitude)
+        })
+    }   
+    
+
+    
+
   return (
     <div>
         <form onSubmit={submit}>
-            <input type="file" onChange={(e)=>{singlefilehandler(e)}} multiple/>
-            {/* <input type= "submit" value = "upload"/> */}
+            <input type="file" onChange={(e)=>{singlefilehandler(e)}} />
+            <p id='error' style={style}></p>
+            <input type= "submit" value = "GET LOCATION" onClick={(e)=>{loc(e)}}/>
         </form>
     </div>
   )
